30185fdabbf3354f04c529bf7973baf652a6617b
Georgi Davidkov <georgi.davidkov@interop.io>
2025-02-15 18:20:13 +0200

    Include error details in CTRF JSON report
    - Added methods to handle test start, end, and individual test results.
    - Implemented methods to update test results and summary in the CTRF report.

---
+++ tests/hooks.spec.js
@@ -0,0 +1,21 @@
+describe('Hooks-after each', () => {
+    describe('Hook should pass', () => {
+        afterEach(() => {
+            console.log('This is an afterEach hook');
+        });
+
+        it('Test 1', () => {});
+
+        it('Test 2', (done) => {
+            done(new Error('This is an error in Test 2'));
+        });
+    });
+
+    describe('Hook should fail', () => {
+        afterEach(() => {
+            throw new Error('This is an error in afterEach hook');
+        });
+
+        it('Test 1', () => { });
+    });
+});
