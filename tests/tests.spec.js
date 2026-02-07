432b6e310e1ba07990dc94136636f6538a765ecf
Matthew Thomas <matthewthomasgb@gmail.com>
2026-02-07 15:28:12 +0000

    fix: remove invalid .describe() chaining in test file

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -5,23 +5,19 @@ describe('Tests', function () {
     done(new Error('This is an error in Test 2'))
   })
 
-  it.skip('should be skipped', function () {}).describe(
-    'Will be marked as pending.'
-  )
+  it.skip('should be skipped', function () {})
 
   context('Tests context', () => {
     beforeEach(() => {
       this.nonExistentProperty.AnotherOneMissing = 123456
     })
 
-    it('should pass', function () {}).describe('Will be marked as skipped.')
+    it('should pass', function () {})
 
     it('should fail', function (done) {
       done(new Error('This is an error in Test 4'))
-    }).describe('Will be marked as skipped.')
+    })
 
-    it.skip('should be skipped', function () {}).describe(
-      'Will be marked as skipped.'
-    )
+    it.skip('should be skipped', function () {})
   })
 })
