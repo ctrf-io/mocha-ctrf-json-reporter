c4bd1ccb90f802f75ad73f12c0beff345b67e58a
Matthew Thomas <matthewthomasgb@gmail.com>
2026-02-09 20:27:53 +0000

    chore: upgrade dev dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist/',
+      'ctrf/',
+      '**/tests/**',
+      '**/jest.config.js',
+      '**/.eslintrc.js',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  }
+)
